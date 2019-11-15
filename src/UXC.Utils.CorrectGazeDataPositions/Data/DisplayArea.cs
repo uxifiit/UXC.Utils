90a828e3f73b5c1cc05ab152a80a842b6a4223cc
Martin Konopka <martkono@outlook.com>
2019-11-15 17:55:59 +0100

    added CorrectGazeDataPositions

---
+++ src/UXC.Utils.CorrectGazeDataPositions/Data/DisplayArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UXC.Core.Data;
+
+namespace UXC.Utils.CorrectTimestamps.Data
+{
+    public class DisplayArea
+    {
+        public DisplayArea(Point3 bottomLeft, Point3 topLeft, Point3 topRight)
+        {
+            BottomLeft = bottomLeft;
+            TopLeft = topLeft;
+            TopRight = topRight;
+        }
+
+        public Point3 BottomLeft { get; }
+        public Point3 TopLeft { get; }
+        public Point3 TopRight { get; }
+    }
+
+}
---
+++ src/UXC.Utils.CorrectGazeDataPositions/Data/TrackBoxCoords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UXC.Core.Data;
+
+namespace UXC.Utils.CorrectGazeDataPositions.Data
+{
+    public class TrackBoxCoords
+    {
+        public Point3 FrontUpperRight { get; set; }
+        public Point3 FrontUpperLeft { get; set; }
+        public Point3 FrontLowerLeft { get; set; }
+        public Point3 FrontLowerRight { get; set; }
+        public Point3 BackUpperRight { get; set; }
+        public Point3 BackUpperLeft { get; set; }
+        public Point3 BackLowerLeft { get; set; }
+        public Point3 BackLowerRight { get; set; }
+    }
+
+}
